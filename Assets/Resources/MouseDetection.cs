6139e405deb9dfe58a014bda372277581147242c
SylvainCapart <sylvain.capart@cfa-afti.fr>
2018-02-08 20:21:11 +0100

    Initial commit

---
+++ Assets/Resources/MouseDetection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnMouseDown()
+    {
+        
+        //Debug.Log("Mouse down");
+        if (Input.GetMouseButtonDown(0))
+        {
+           // Debug.Log("Mouse 0 down");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Debug.Log("Mouse position : " + Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+               // Debug.Log("Raycast hit");
+                if (hit.transform.tag == "Squirrel")
+                {
+                    Debug.Log("DestroYED");
+                    GameObject.Find("SquirrelMgt").GetComponent<AudioSource>().Play();
+                    SquirrelMgt.squirrelCount--;
+                    DestroyObject(gameObject);
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Resources/MoveOutSide.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOutSide : MonoBehaviour {
+
+    public int speed = 10;
+
+    private bool left;
+    private bool right;
+
+	// Use this for initialization
+	void Start () {
+        left = false;
+        right = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("a") || Input.GetKeyDown("q") || Input.GetKeyDown("left"))
+        {
+            left = true;
+        }
+        else if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
+        {
+            right = true;
+        }
+        else
+        {
+            //Do nothing
+        }
+
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("q") || Input.GetKeyUp("left") )
+        {
+            left = false;
+        }
+        else if (Input.GetKeyUp("d") || Input.GetKeyUp("right") )
+        {
+            right = false;
+        }
+        else
+        {
+            //Do nothing
+        }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (right && transform.position.x > -4.5f)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        else if (left && transform.position.x < 4.5f)
+             transform.Translate(Vector3.right * Time.deltaTime * speed);
+        else
+        {
+            // Do nothing
+        }
+    }
+}
